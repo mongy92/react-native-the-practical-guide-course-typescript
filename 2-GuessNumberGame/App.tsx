a0e6d21717cff59b60e6ec487942c39ace414895
Ahmed Mongy <a.elzantot@gmail.com>
2022-12-06 12:14:28 +0200

    chapter4:install and use linear gradient

--- 2-GuessNumberGame/App.tsx
+++ 2-GuessNumberGame/App.tsx
@@ -1,20 +1,17 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.container}>
+      <StatusBar style='auto' />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
---
+++ 2-GuessNumberGame/screens/StartGameScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <Text>StartGameScreen</Text>
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({});

f968fef32ca40d6a759404165615d95ba2ed0c6a
Ahmed Mongy <a.elzantot@gmail.com>
2022-12-14 11:59:05 +0200

    chapter6: write test cases for categories screen

--- 3-MealsApp/src/screens/Categories/__tests__/Categories.test.tsx
+++ 3-MealsApp/src/screens/Categories/__tests__/Categories.test.tsx
@@ -1,7 +1,20 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { render, screen } from '@testing-library/react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { fireEvent, render } from '@testing-library/react-native';
 import { testIDs } from '../../../constants/testIDs';
+import { mockedCategories } from '../../../mocks/mocked-categories';
 import CategoriesScreen from '../index';
+
+const mockedNavigate = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: mockedNavigate
+    })
+  };
+});
 
 const Component = (
   <NavigationContainer>
@@ -12,5 +25,24 @@ describe('CategoriesScreen', () => {
   test('should render correctly', async () => {
     const { getByTestId } = render(Component);
     expect(getByTestId(testIDs.categoriesScreen));
+  });
+
+  test('should render list of categories', () => {
+    const { getByText } = render(Component);
+    mockedCategories.forEach((c) => {
+      getByText(c.title);
+    });
+  });
+
+  test('should navigate to Meals screen on press category ', () => {
+    const navigation = useNavigation();
+    const { getByText } = render(Component);
+    const categoryItem = getByText(mockedCategories[0].title);
+
+    fireEvent.press(categoryItem);
+
+    expect(navigation.navigate).toHaveBeenCalledWith('Meals', {
+      categoryId: mockedCategories[0].id
+    });
   });
 });

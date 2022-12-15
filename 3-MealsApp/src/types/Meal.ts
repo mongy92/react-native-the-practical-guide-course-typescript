3ab3dbaa010250a2f2901b49f70abbf185e3f418
Ahmed Mongy <a.elzantot@gmail.com>
2022-12-15 12:55:23 +0200

    chapter6: prepare types and data for meals

---
+++ 3-MealsApp/src/types/Meal.ts
@@ -0,0 +1,15 @@
+export type Meal = {
+  id: string;
+  categoryIds: string[];
+  title: string;
+  affordability: string;
+  complexity: string;
+  imageUrl: string;
+  duration: number;
+  ingredients: string[];
+  steps: string[];
+  isGlutenFree: boolean;
+  isVegan: boolean;
+  isVegetarian: boolean;
+  isLactoseFree: boolean;
+};

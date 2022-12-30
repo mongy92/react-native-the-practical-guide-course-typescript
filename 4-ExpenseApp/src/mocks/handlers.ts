770b0f49ab3f979b1b7ffe74a564fd4e522ee2c4
Ahmed Mongy <a.elzantot@gmail.com>
2022-12-30 13:59:36 +0200

    chapter11: write tests for ManageExpense

--- 4-ExpenseApp/src/mocks/handlers.ts
+++ 4-ExpenseApp/src/mocks/handlers.ts
@@ -1,12 +1,18 @@
 import { rest } from 'msw';
 import { BASE_URL } from '../utils/http';
 import { mockedExpenses } from './mockedExpenses';
-
+const matcher = `${BASE_URL}/expenses.json`;
 export const handlers = [
-  rest.get(`${BASE_URL}/expenses.json`, (req, res, ctx) => {
+  rest.get(matcher, (req, res, ctx) => {
     return res(ctx.json(mockedExpenses));
   }),
-  rest.post('/expenses', (req, res, ctx) => res(ctx.status(201))),
-  rest.put('/expenses', (req, res, ctx) => res(ctx.status(200))),
-  rest.delete('/expenses', (req, res, ctx) => res(ctx.status(200)))
+  rest.post(matcher, (req, res, ctx) =>
+    res(ctx.status(201), ctx.json({ name: 'id' }))
+  ),
+  rest.put(`${BASE_URL}/expenses/:id.json`, (req, res, ctx) =>
+    res(ctx.status(200))
+  ),
+  rest.delete(`${BASE_URL}/expenses/:id.json`, (req, res, ctx) =>
+    res(ctx.status(200))
+  )
 ];
